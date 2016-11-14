06df6fed133e4157433f5df838e1b23912c2dc82
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2016-11-14 09:55:15 +0600

    Update ACS712.ino

--- ACS712.ino
+++ ACS712.ino
@@ -6,7 +6,9 @@ put ACS712.cpp and ACS712.h files in the same folder
 
 #include "ACS712.h"
 
-ACS712 ac1(A0);
+float error=0.05f;
+int sensorPin=A0;
+ACS712 ac1(sensorPin, error);
 
 double current=0;
 
